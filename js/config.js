e4fc6109c1da8291c59417c410e07ab81fd07db7
r1shu-R <iamworstpro@gmail.com>
2026-04-08 23:26:36 +0530

    Ignore stale localhost API base on hosted site

--- js/config.js
+++ js/config.js
@@ -5,6 +5,16 @@
       ? window.location.origin
       : "http://127.0.0.1:8000";
 
-  const baseOrigin = (storedBase || runtimeOrigin).replace(/\/+$/, "");
+  const isHostedRuntime = /^https:\/\//i.test(runtimeOrigin);
+  const isLocalStoredBase =
+    !!storedBase && /localhost|127\.0\.0\.1/i.test(storedBase);
+
+  if (isHostedRuntime && isLocalStoredBase) {
+    window.localStorage.removeItem("smart_campus_api_base");
+  }
+
+  const baseOrigin = (
+    isHostedRuntime && isLocalStoredBase ? runtimeOrigin : (storedBase || runtimeOrigin)
+  ).replace(/\/+$/, "");
   window.SMART_CAMPUS_API = `${baseOrigin}/api`;
 })();
